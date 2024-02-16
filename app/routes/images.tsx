e15863a885184ce65b0b9277e30c8c2e20a28848
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-02-16 16:52:43 +0100

    add layout

---
+++ app/routes/images.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "~/components/layout";
+
+export default function Images() {
+  return (
+    <Layout>
+      <h2 className="text-yellow-300 font-extrabold text-5xl">Image Route</h2>
+    </Layout>
+  );
+}
---
+++ app/routes/panoramas.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "~/components/layout";
+
+export default function Panoramas() {
+  return (
+    <Layout>
+      <h2 className="text-yellow-300 font-extrabold text-5xl">
+        Panorama Route
+      </h2>
+    </Layout>
+  );
+}
