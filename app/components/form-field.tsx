a54573ed41c05b614700aefd20a9edf27479df1a
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2024-02-20 20:05:04 +0100

    add error handling to form-field

--- app/components/form-field.tsx
+++ app/components/form-field.tsx
@@ -1,13 +1,11 @@
+import { useEffect, useState } from "react";
 interface FormFieldProps {
   htmlFor: string;
-
   label: string;
-
   type?: string;
-
   value: any;
-
   onChange?: (...args: any) => any;
+  error?: string;
 }
 
 export function FormField({
@@ -16,21 +14,34 @@ export function FormField({
   type = "text",
   value,
   onChange = () => {},
+  error = "",
 }: FormFieldProps) {
+  const [errorText, setErrorText] = useState(error);
+
+  useEffect(() => {
+    setErrorText(error);
+  }, [error]);
+
   return (
     <>
       <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
         {label}
       </label>
-
       <input
-        onChange={onChange}
+        onChange={(e) => {
+          onChange(e);
+          setErrorText("");
+        }}
         type={type}
         id={htmlFor}
         name={htmlFor}
         className="w-full p-2 rounded-xl my-2"
         value={value}
       />
+
+      <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+        {errorText || ""}
+      </div>
     </>
   );
 }
